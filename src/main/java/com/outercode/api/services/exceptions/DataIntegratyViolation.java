74f76917b2159e4beb8809e464c2e40a57310846
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2024-09-04 00:23:35 -0300

    exception class

---
+++ src/main/java/com/outercode/api/services/exceptions/DataIntegratyViolation.java
@@ -0,0 +1,6 @@
+package com.outercode.api.services.exceptions;
+
+public class DataIntegratyViolation extends RuntimeException{
+
+    public DataIntegratyViolation(String message) {super(message);}
+}
